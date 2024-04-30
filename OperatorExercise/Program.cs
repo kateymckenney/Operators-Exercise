4e4f25a2b42544483a9ff1ca1a2921ab0155f759
Katey McKenney <mckenneykatey@gmail.com>
2024-04-30 14:24:11 -0500

    completed exercise 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace OperatorExercise
+﻿using System;
+
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //----------EXERCISE ONE------------------------
+
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int subract = a - b;
+            int multiply = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine();
+
+
+            //--------------EXERCISE TWO------------------
+
+
+            Console.WriteLine("Enter the radius of your circle.");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+
+
         }
     }
 }
